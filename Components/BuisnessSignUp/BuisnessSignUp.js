174785f9e9793631928bc4162f7e1c55db0ff883
maham-sheikh <l211845@lhr.nu.edu.pk>
2025-04-28 20:22:46 +0500

    commit chnages

--- Components/BuisnessSignUp/BuisnessSignUp.js
+++ Components/BuisnessSignUp/BuisnessSignUp.js
@@ -1,39 +1,39 @@
-// import React from 'react';
-// import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-// import { useNavigation } from '@react-navigation/native'; // Import the useNavigation hook
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native'; // Import the useNavigation hook
 
-// function BusinessSignUp() {
-//   const navigation = useNavigation(); 
-
-
-//   const navigateToServices = () => {
-//     navigation.navigate('VendorSignup'); 
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <TouchableOpacity onPress={navigateToServices} activeOpacity={0.7}>
-//         <Text style={styles.text}>Welcome to the Business SignUp page!</Text>
-//       </TouchableOpacity>
-//     </View>
-//   );
-// }
+function BusinessSignUp() {
+  const navigation = useNavigation(); 
 
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F1FFF3',
-//     paddingHorizontal: 20,
-//   },
-//   text: {
-//     fontSize: 20,
-//     fontWeight: '600',
-//     color: '#00B1D0',
-//     textAlign: 'center',
-//   },
-// });
+  const navigateToServices = () => {
+    navigation.navigate('VendorSignup'); 
+  };
 
-// export default BusinessSignUp;
+  return (
+    <View style={styles.container}>
+      {/* <TouchableOpacity onPress={navigateToServices} activeOpacity={0.7}>
+        <Text style={styles.text}>Welcome to the Business SignUp page!</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F1FFF3',
+    paddingHorizontal: 20,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#00B1D0',
+    textAlign: 'center',
+  },
+});
+
+export default BusinessSignUp;
